710d4229fcfda3af43b535d58babbf0b9acddc56
Salem Code <salemcode8@gmail.com>
2022-10-07 10:27:00 +0200

    Finish Monitoring

--- app/bundles/CoreBundle/Templating/Twig/Extension/ObjectExtension.php
+++ app/bundles/CoreBundle/Templating/Twig/Extension/ObjectExtension.php
@@ -8,7 +8,8 @@ use Twig\TwigTest;
 class ObjectExtension extends AbstractExtension {
     public function getTests(){
         return [
-            new TwigTest('object', fn($value) => is_object($value))
+            new TwigTest('object', fn($value) => is_object($value)),
+            new TwigTest('array', fn($value) => is_array($value))
         ];
     }
 }
